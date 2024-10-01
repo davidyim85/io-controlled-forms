c5222887bfb05f9e0587d34077c612442e4b1ff5
syandbb <syandbb@syandbbs-MacBook-Air.local>
2024-10-01 18:55:06 -0400

    added code for submit and onchange

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import { useState } from "react";
 
 const App = () => {
      const [formData, setFormData] = useState({
-        firstName: '',
+        firstName: '', 
         lastName: ''
      })
+     const [title, setTitle] = useState('full name will appear here')
+     console.log(formData)
     return (
+        <>
+        <h2>{title}</h2>
         <form 
             onSubmit={(e) => {
                 e.preventDefault();
+                setTitle(`form submitted with values ${formData.firstName} & ${formData.lastName}`)
                 console.log(formData)
             }}
         >
@@ -17,10 +22,11 @@ const App = () => {
             <input
                 name='firstName'
                 value={formData.firstName}
-                onChange={event => {
+                onChange={e => {
+                    console.log(e.target)
                     setFormData({
-                        ...formData,
-                        [event.target.name]: event.target.value
+                        ...formData,//we have 2 properties in this object. This spread in the object means "do not write over the other properties" that are not mentioned
+                        [e.target.name]: e.target.value
                     })
                 }}
             />
@@ -31,14 +37,16 @@ const App = () => {
                 name='lastName'
                 value={formData.lastName}
                 onChange={event => {
+                    console.log(event.target)
                     setFormData({
-                        ...formData,
+                        ...formData,//we have 2 properties in this object. This spread in the object means "do not write over the other properties" that are not mentioned
                         [event.target.name]: event.target.value
                     })
                 }}
             />
             <button type='submit'>Submit</button>
         </form>
+        </>
     )
 }
 
